9eb382df5e241ed8c9dda67b1e8719ff94adbb34
OriolQP <oriolquintanilla2@gmail.com>
2022-05-12 17:54:14 +0200

    dc

--- cms_avanzado/views/nav/main_admin_nav.php
+++ cms_avanzado/views/nav/main_admin_nav.php
@@ -6,7 +6,7 @@
     Categorias
   </a>
   <div class="right menu">
-    <a href="http://localhost/Projectes/GS/repte GuillemRigol/cms_avanzado/log-out" class="item">
+    <a href="http://localhost/Projectes/GS/repte GuillemRigol/cms_avanzado/res/php/user_actions/log-out.php" class="item">
       Log Out
     </a>
   </div>
